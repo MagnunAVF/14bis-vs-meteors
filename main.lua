2a79fdabc164684719eda62c903fcc991a956ccc
Magnun A V F <magnunavf@gmail.com>
2021-07-15 23:22:12 -0300

    Create 14Bis airplane

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
+airplane_14bis = {
+    src = "images/14bis.png",
+    width = 64,
+    height = 64,
+    x = 0,
+    y = 0
+}
+
 -- Load some default values for our rectangle.
 function love.load()
     love.window.setMode(320, 480, { resizable = false })
     love.window.setTitle("14Bis vs Meteors")
 
     background = love.graphics.newImage("images/background.png")
-
-    x, y, w, h = 20, 20, 60, 20
+    airplane_14bis.image = love.graphics.newImage(airplane_14bis.src)
 end
 
 -- Increase the size of the rectangle every frame.
@@ -17,5 +24,5 @@ end
 function love.draw()
     love.graphics.draw(background, 0, 0)
 
-    love.graphics.rectangle("fill", x, y, w, h)
+    love.graphics.draw(airplane_14bis.image, airplane_14bis.x, airplane_14bis.y)
 end
