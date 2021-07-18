de0b6db750796a0a5a5ccf55194a10dfef6958ad
Magnun A V F <magnunavf@gmail.com>
2021-07-17 23:32:12 -0300

    Improve meteors movement

--- main.lua
+++ main.lua
@@ -15,14 +15,17 @@ meteors = {}
 function create_meteor()
     meteor = {
         x = math.random(SCREEN_WIDTH),
-        y = 0
+        y = -70,
+        weight = math.random(3),
+        horizontal_movement = math.random(-1,1)
     }
     table.insert( meteors, meteor)
 end
 
 function move_meteors()
     for _, meteor in pairs(meteors) do
-        meteor.y = meteor.y + 1
+        meteor.y = meteor.y + meteor.weight
+        meteor.x = meteor.x + meteor.horizontal_movement
     end
 end
 
@@ -52,6 +55,8 @@ end
 function love.load()
     love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, { resizable = false })
     love.window.setTitle("14Bis vs Meteors")
+
+    math.randomseed(os.time())
 
     background = love.graphics.newImage("images/background.png")
     airplane_14bis.image = love.graphics.newImage(airplane_14bis.src)
