f78fe002acb7c721f771d34bb94efa57ac9830bd
Magnun A V F <magnunavf@gmail.com>
2021-07-17 23:57:45 -0300

    Create collision detection and Game Over

--- main.lua
+++ main.lua
@@ -4,11 +4,25 @@ MAX_METEORS = 12
 
 airplane_14bis = {
     src = "images/14bis.png",
-    width = 64,
-    height = 64,
+    width = 55,
+    height = 63,
     x = SCREEN_WIDTH/2 - 64/2,
     y = SCREEN_HEIGHT - 64/2
 }
+
+function destroy_airplane()
+    airplane_14bis.src = "images/explosion.png"
+    airplane_14bis.image = love.graphics.newImage(airplane_14bis.src)
+    airplane_14bis.width = 67
+    airplane_14bis.height = 77
+end
+
+function has_collision(x1, y1, w1, h1, x2, y2, w2, h2)
+    return x2 < x1 + w1 and
+        x1 < x2 + w2 and
+        y1 < y2 + h2 and
+        y2 < y1 + h1
+end
 
 meteors = {}
 
@@ -16,6 +30,8 @@ function create_meteor()
     meteor = {
         x = math.random(SCREEN_WIDTH),
         y = -70,
+        width = 50,
+        height = 44,
         weight = math.random(3),
         horizontal_movement = math.random(-1,1)
     }
@@ -52,6 +68,16 @@ function move_14bis()
     end
 end
 
+function check_collisions()
+    for _, meteor in pairs(meteors) do
+        if has_collision(meteor.x, meteor.y, meteor.width, meteor.height,
+            airplane_14bis.x, airplane_14bis.y, airplane_14bis.width, airplane_14bis.height) then
+            destroy_airplane()
+            GAME_OVER = true
+        end
+    end
+end
+
 function love.load()
     love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT, { resizable = false })
     love.window.setTitle("14Bis vs Meteors")
@@ -64,15 +90,21 @@ function love.load()
 end
 
 function love.update(dt)
-    if love.keyboard.isDown('w', 'a', 's', 'd') then
-        move_14bis()
-    end
+    if not GAME_OVER then
+        if love.keyboard.isDown('w', 'a', 's', 'd') then
+            move_14bis()
+        end
 
-    remove_meteors()
-    if #meteors < MAX_METEORS then
-        create_meteor()
+        remove_meteors()
+
+        if #meteors < MAX_METEORS then
+            create_meteor()
+        end
+
+        move_meteors()
+
+        check_collisions()
     end
-    move_meteors()
 end
 
 function love.draw()
